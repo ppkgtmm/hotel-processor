6c800239607b99e3fa48b7730e6817ee8bea2b5b
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-02 21:33:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.sql import text
+from os import getenv, listdir, path
+from google.cloud.bigquery import Client, Table
 
-def handler(event, context):
-    username, password = getenv("DWH_USER"), getenv("DWH_PASSWORD")
-    host, database = getenv("DWH_HOST"), getenv("DWH_NAME")
-    engine = create_engine(
-        f"redshift+redshift_connector://{username}:{password}@{host}/{database}"
-    )
-    conn = engine.connect()
-    with open("warehouse.sql", "r") as fp:
-        for query in fp.read().split(";"):
-            query = query.strip("\n\t\r ")
-            if query == "":
-                continue
-            conn.execute(text(query))
-    conn.close()
-    engine.dispose()
+
+def prepare_bq_storage(dataset_name: str, schema_dir: str):
+    bq_client = Client()
+    dataset_ref = getenv("GCP_PROJECT_ID") + "." + dataset_name
+    bq_client.create_dataset(dataset_ref, exists_ok=True)
+    for file_name in listdir(schema_dir):
+        file_path = path.join(schema_dir, file_name)
+        table_name = file_name.split(".")[0]
+        table = Table(
+            dataset_ref + "." + table_name, schema=bq_client.schema_from_json(file_path)
+        )
+        bq_client.delete_table(table, not_found_ok=True)
+        bq_client.create_table(table)
+
+
+def prepare_and_submit(request):
+    prepare_bq_storage("staging", "schemas/staging")
+    prepare_bq_storage("warehouse", "schemas/warehouse")
     return "success"
