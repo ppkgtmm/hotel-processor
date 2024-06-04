0a0f6828d42efb96ece1421c87e05389e56583ad
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-04 23:17:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from google.cloud.bigquery_datatransfer import (
     TransferConfig,
     CreateTransferConfigRequest,
 )
-
 from google.cloud.dataproc import JobControllerClient, Job, PySparkJob, JobPlacement
+
 
 def prepare_bq_storage(dataset_name: str, schema_dir: str):
     bq_client = Client()
@@ -45,9 +45,47 @@ def schedule_sql_query(folder_path: str, schedule_interval: str):
         )
 
 
+def submit_streaming_job():
+    job_client = JobControllerClient(
+        client_options={
+            "api_endpoint": f"{getenv("GCP_REGION")}-dataproc.googleapis.com:443"
+        }
+    )
+    pyspark_job = PySparkJob()
+    pyspark_job.main_python_file_uri = f"gs://{getenv("BUCKET_NAME")}/pyspark/main.py"
+    pyspark_job.python_file_uris = [
+        f"gs://{getenv("BUCKET_NAME")}/pyspark/dependencies.zip"
+    ]
+    pyspark_job.jar_file_uris = [
+        "gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.39.0.jar",
+        "gs://spark-lib/pubsublite/pubsublite-spark-sql-streaming-LATEST-with-dependencies.jar",
+        "gs://hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar",
+    ]
+    pyspark_job.properties = {
+        "dataproc:pip.packages": "db-dtypes==1.2.0,google-cloud-bigquery==3.23.1,pandas==2.2.2",
+        "spark.pyspark.python": "python3.12",
+        "spark.pyspark.driver.python": "python3.12",
+        "spark:spark.submit.deployMode": "cluster",
+        "spark.yarn.appMasterEnv.GCP_PROJECT": getenv("GCP_PROJECT_ID"),
+        "spark.yarn.appMasterEnv.GCP_ZONE": getenv("GCP_ZONE"),
+        "spark.yarn.appMasterEnv.GCS_BUCKET": getenv("BUCKET_NAME"),
+        "spark.executorEnv.GCP_PROJECT": getenv("GCP_PROJECT_ID"),
+        "spark.executorEnv.GCP_ZONE": getenv("GCP_ZONE"),
+        "spark.executorEnv.GCS_BUCKET": getenv("BUCKET_NAME"),
+    }
+    job = Job()
+    job.placement = JobPlacement(cluster_name=getenv("CLUSTER_NAME"))
+    job.pyspark_job = pyspark_job
+
+    job_client.submit_job(
+        project_id=getenv("GCP_PROJECT_ID"), region=getenv("GCP_REGION"), job=job
+    )
+
+
 def prepare_and_process(request):
     prepare_bq_storage("staging", "schemas/staging")
     prepare_bq_storage("warehouse", "schemas/warehouse")
     schedule_sql_query("queries/warehouse", "every day 00:00")
     schedule_sql_query("queries/staging", "every day 01:00")
+    submit_streaming_job()
     return "success"
