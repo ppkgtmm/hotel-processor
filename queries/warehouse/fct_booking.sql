43ed48d9f6adc9304816582fcd210350dd83ce76
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-05 16:21:57 +0700

    Update fct_booking.sql

--- queries/warehouse/fct_booking.sql
+++ queries/warehouse/fct_booking.sql
@@ -7,57 +7,49 @@ CREATE TEMPORARY TABLE fact_booking AS
             br.room,
             br.guest,
             br.updated_at
-        FROM staging.booking_room br
-        JOIN staging.booking b
-        ON br.booking = b.id
+        FROM staging.booking_room br JOIN staging.booking b ON br.booking = b.id
         WHERE br.is_deleted = false 
         AND br.date_processed IS NULL 
         AND b.is_deleted = false 
         AND DATE_DIFF(b.checkin, DATE_ADD(DATE(@run_time), INTERVAL 1 DAY), DAY) < 7
+    ), dim_guest AS (
+        SELECT b.id AS booking, MAX(g.id) AS id
+        FROM raw_bookings b JOIN warehouse.dim_guest g ON g._id = b.guest AND g.effective_from <= b.updated_at
+        GROUP BY booking
+    ), guest_location AS (
+        SELECT b.id AS booking, b.updated_at, ARRAY_AGG(g.location ORDER BY g.updated_at DESC LIMIT 1)[SAFE_OFFSET(0)] AS location
+        FROM raw_bookings b JOIN staging.guest g ON g.id = b.guest AND g.updated_at <= b.updated_at
+        GROUP BY booking, b.updated_at
+    ), dim_location AS (
+        SELECT booking, MAX(l.id) AS id
+        FROM guest_location b JOIN warehouse.dim_location l ON l._id = b.location AND l.effective_from <= b.updated_at
+        GROUP BY booking
+    ), roomtype AS (
+        SELECT b.id AS booking, b.updated_at, ARRAY_AGG(r.roomtype ORDER BY r.updated_at DESC LIMIT 1)[SAFE_OFFSET(0)] AS roomtype
+        FROM raw_bookings b JOIN staging.room r ON r.id = b.room AND r.updated_at <= b.updated_at
+        GROUP BY booking, b.updated_at
+    ), dim_roomtype AS (
+        SELECT booking, MAX(t.id) AS id
+        FROM roomtype b JOIN warehouse.dim_roomtype t ON t._id = b.roomtype AND t.effective_from <= b.updated_at
+        GROUP BY booking
     ), bookings AS (
         SELECT
             b.id,
             b.checkin,
             b.checkout,
             b.updated_at,
-            (
-                SELECT MAX(id)
-                FROM warehouse.dim_guest
-                WHERE _id = b.guest AND effective_from <= b.updated_at
-            ) AS guest,
-            (
-                SELECT g.location
-                FROM staging.guest g
-                WHERE g.id = b.guest AND g.updated_at <= b.updated_at
-                ORDER BY g.updated_at DESC
-                LIMIT 1
-            ) AS guest_location,
-            (
-                SELECT roomtype
-                FROM staging.room r
-                WHERE r.id = b.room AND r.updated_at <= b.updated_at
-                ORDER BY r.updated_at DESC
-                LIMIT 1
-            ) AS roomtype
-        FROM raw_bookings b
+            g.id AS guest,
+            l.id AS guest_location,
+            t.id AS roomtype
+        FROM raw_bookings b JOIN dim_guest g ON b.id = g.booking
+        JOIN dim_location l ON b.id = l.booking
+        JOIN dim_roomtype t ON b.id = t.booking
     ), enriched_bookings AS (
         SELECT
-            b.id,
-            b.guest,
-            (
-                SELECT MAX(id)
-                FROM warehouse.dim_location
-                WHERE _id = b.guest_location AND effective_from <= b.updated_at
-            ) AS guest_location,
-            (
-                SELECT MAX(id)
-                FROM warehouse.dim_roomtype
-                WHERE _id = b.roomtype AND effective_from <= b.updated_at
-            ) AS roomtype,
+            b.* EXCEPT(checkin, checkout, updated_at),
             date
         FROM bookings b,
         UNNEST(GENERATE_DATE_ARRAY(b.checkin, b.checkout)) AS date
-        WHERE b.guest IS NOT NULL AND b.guest_location IS NOT NULL AND b.roomtype IS NOT NULL
     )
 
 SELECT 
@@ -67,7 +59,6 @@ SELECT
     guest_location,
     roomtype
 FROM enriched_bookings
-WHERE guest_location IS NOT NULL AND roomtype IS NOT NULL;
 
 INSERT INTO warehouse.fct_booking (date, guest, guest_location, roomtype)
 SELECT date, guest, guest_location, roomtype
