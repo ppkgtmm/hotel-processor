9a81a1c9a5fa739450daab1d2c01c43b8533416a
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-06 23:41:13 +0700

    Update dim_date.sql

--- queries/warehouse/dim_date.sql
+++ queries/warehouse/dim_date.sql
@@ -1,9 +1,3 @@
-WITH old_date AS (
-    SELECT COALESCE(
-        (SELECT MAX(date) FROM warehouse.dim_date),
-        (SELECT MIN(checkin) FROM staging.booking WHERE is_deleted = false)
-    ) AS start_date
-)
 MERGE INTO warehouse.dim_date t
 USING (
     SELECT 
@@ -12,7 +6,12 @@ USING (
     EXTRACT(MONTH FROM date) AS month, 
     EXTRACT(QUARTER FROM date) AS quarter, 
     EXTRACT(YEAR FROM date) AS year
-    FROM UNNEST(GENERATE_DATE_ARRAY((SELECT start_date FROM old_date), DATE(@run_time))) AS date
+    FROM UNNEST(GENERATE_DATE_ARRAY((SELECT start_date FROM (
+      SELECT COALESCE(
+        (SELECT MAX(date) FROM warehouse.dim_date),
+        (SELECT MIN(checkin) FROM staging.booking WHERE is_deleted = false)
+    ) AS start_date
+    )), DATE(@run_time))) AS date
 ) s
 ON t.id = s.id
 WHEN NOT MATCHED THEN INSERT ROW;
